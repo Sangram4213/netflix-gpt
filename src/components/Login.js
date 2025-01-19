af9a4db76150d88b663e78dc4658d46943a15ef8
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2025-01-19 12:03:58 +0530

    add a video background

--- src/components/Login.js
+++ src/components/Login.js
@@ -75,7 +75,6 @@ const Login = () => {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
